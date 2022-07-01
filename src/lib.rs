1df53ed0bb038c15c4371bbbc037462d3fcd2dca
yijiahe <yi.jiahe00@gmail.com>
2022-07-01 02:40:20 +0000

    add update function to Body2D

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,17 @@ use wasm_bindgen::prelude::*;
 extern crate nalgebra as na;
 
 pub struct Body2D {
-    position: na::Vector2<f32>,
+    mass: f32,
+    position: na::Point2<f32>,
+    velocity: na::Vector2<f32>,
+    acceleration: na::Vector2<f32>,
 }
 
 impl Body2D {
-
+    fn update(&mut self, time_delta: f32) {
+        self.velocity *= na::Vector1::new(f32::powi(time_delta, 2));
+        self.position += self.velocity * na::Vector1::new(time_delta);
+    }
 }
 
 #[wasm_bindgen]
