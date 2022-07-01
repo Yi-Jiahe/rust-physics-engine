036fde9d354bd3fa8b7864a28af3e747b16d69f5
yijiahe <yi.jiahe00@gmail.com>
2022-07-01 03:47:14 +0000

    [WIP] Implement world update method

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,27 @@ use wasm_bindgen::prelude::*;
 
 extern crate nalgebra as na;
 
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct World2D {
+    bodies: Vec<Body2D>,
+}
+
+#[wasm_bindgen]
+impl World2D {
+    pub fn add(&mut self, body: Body2D) {
+        self.bodies.push(body);
+    }
+
+    pub fn update(&mut self, time_delta: f32) {
+        for &mut body in self.bodies {
+            body.update(time_delta);
+        }
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Debug)]
 pub struct Body2D {
     mass: f32,
     position: na::Point2<f32>,
@@ -25,9 +46,20 @@ impl Body2D {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let mut world = World2D {bodies: Vec::new()};
+        world.add(Body2D {
+            mass: 1.0,
+            position: na::Point2::new(0.0, 0.0),
+            velocity: na::Vector2::new(1.0, 2.0),
+            acceleration: na::Vector2::new(0.0, 0.0),
+        });
+
+        world.update(0.1);
+
+        dbg!(world);
     }
 }
