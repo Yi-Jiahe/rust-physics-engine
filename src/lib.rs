cfc8128720b4171b88476aa8f90e25e8d85cee63
yijiahe <yi.jiahe00@gmail.com>
2022-07-13 08:17:38 +0000

    Implement new for Body2D and update tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,8 @@ pub struct World2D {
 #[wasm_bindgen]
 impl World2D {
     pub fn new() -> World2D {
-        let bodies: Vec<Body2D> = Vec::new();
-
         World2D {
-            bodies,
+            bodies: Vec::<Body2D>::new(),
         }
     }
 
@@ -49,7 +47,14 @@ impl Body2D {
 
 #[wasm_bindgen]
 impl Body2D {
-
+    pub fn new(mass: f32, position: Vec<f32>) -> Body2D {
+        Body2D {
+            mass,
+            position: na::point![position[0], position[1]],
+            velocity: na::vector![0.0, 0.0],
+            acceleration: na::vector![0.0, 0.0],
+        }
+    }
 }
 
 #[cfg(test)]
@@ -58,13 +63,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut world = World2D {bodies: Vec::new()};
-        world.add(Body2D {
-            mass: 1.0,
-            position: na::Point2::new(0.0, 0.0),
-            velocity: na::Vector2::new(1.0, 2.0),
-            acceleration: na::Vector2::new(0.0, 0.0),
-        });
+        let mut world = World2D::new();
+        world.add(Body2D::new(1.0, vec![0.0, 0.0]));
 
         world.update(0.1);
 
