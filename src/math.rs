5f35759fca38a747e1ddb9ac4eb777f894c37d9e
yijiahe <yi.jiahe00@gmail.com>
2022-07-15 03:24:31 +0000

    Add constructor to f32Vector2

--- src/math.rs
+++ src/math.rs
@@ -9,3 +9,14 @@ pub struct f32Vector2 {
     pub x: f32,
     pub y: f32,
 }
+
+#[wasm_bindgen]
+impl f32Vector2 {
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: f32, y: f32) -> f32Vector2 {
+        f32Vector2 {
+            x,
+            y,
+        }
+    }
+}
